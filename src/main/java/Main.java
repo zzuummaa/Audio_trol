4c4106514930266c735a828fc956700fdc5a845b
zzuummaa <stepannnk@mail.ru>
2017-07-22 15:29:29 +0300

    First normal commit. Now accurately.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,10 +2,7 @@ import org.opencv.core.Core;
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfRect;
 import utils.ImageMarker;
-
-import java.awt.image.BufferedImage;
-import java.awt.image.DataBufferByte;
-import java.awt.image.WritableRaster;
+import utils.ImageProcessor;
 
 /**
  * Created by Fomenko_S.V. on 21.07.2017.
@@ -25,6 +22,7 @@ public class Main {
     public void detImg() throws InterruptedException {
         Vision vision = new Vision();
         ImageMarker marker = new ImageMarker();
+        ImageProcessor imageProcessor = new ImageProcessor();
 
         while (display.isShowing()){
             if (!vision.look()) {
@@ -37,29 +35,12 @@ public class Main {
             Mat image = vision.getImage();
             marker.markRects(image, diceDetections);
 
-            display.showImage(matToBufferedImage(image));
+            display.showImage(imageProcessor.toBufferedImage(image));
         }
 
         vision.realize();
         display.dispose();
 
-    }
-
-    public static BufferedImage matToBufferedImage(Mat frame) {
-        //Mat() to BufferedImage
-        int type = 0;
-        if (frame.channels() == 1) {
-            type = BufferedImage.TYPE_BYTE_GRAY;
-        } else if (frame.channels() == 3) {
-            type = BufferedImage.TYPE_3BYTE_BGR;
-        }
-        BufferedImage image = new BufferedImage(frame.width(), frame.height(), type);
-        WritableRaster raster = image.getRaster();
-        DataBufferByte dataBuffer = (DataBufferByte) raster.getDataBuffer();
-        byte[] data = dataBuffer.getData();
-        frame.get(0, 0, data);
-
-        return image;
     }
 
 }
