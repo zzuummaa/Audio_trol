5cdbcd4e7feab76f2bc7186fd1ee6f7fe24bed4c
Stephan <stepannk@mail.ru>
2018-04-14 00:06:31 +0300

    Optimazied AsyncClassifier

--- src/main/java/ru/zuma/video/VideoSourceInterface.java
+++ src/main/java/ru/zuma/video/VideoSourceInterface.java
@@ -2,11 +2,16 @@ package ru.zuma.video;
 
 import static org.bytedeco.javacpp.opencv_core.*;
 
-public interface VideoSourceInterface {
+public interface VideoSourceInterface extends AutoCloseable {
 
     boolean isOpened();
 
     Mat grab();
 
     void release();
+
+    @Override
+    default void close() {
+        release();
+    }
 }
